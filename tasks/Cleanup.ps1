4ef789d94a90ca33c5de18d28c108043d3aadb81
Stefano Cocchi <kntrashcan@gmail.com>
2017-02-27 00:30:46 +0000

    Changed cleanup task not to use wildcards

--- tasks/Cleanup.ps1
+++ tasks/Cleanup.ps1
@@ -3,7 +3,7 @@ Task Cleanup {
         
     if(Test-Path $buildPath){
         Write-Host "Cleaning $buildPath"    
-        Remove-Item -Recurse $buildPath\*
+        Get-ChildItem $buildPath | ForEach { Remove-Item $_.FullName -Recurse -Force }
     }else{
         Write-Host "Creating $buildPath"  
         New-Item -ItemType directory $buildPath    
