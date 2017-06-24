a9966b235720f5748b69e2ee123a1e6166aeee24
Stefano Cocchi <kntrashcan@gmail.com>
2017-06-24 07:37:37 +0100

    Modified dotnetcore tasks to use portable runtime

--- tasks/DotNetCore.ps1
+++ tasks/DotNetCore.ps1
@@ -1,11 +1,11 @@
 $Local:dotnetConfiguration = "Release"
 $Local:dotnetFramework = "netcoreapp1.1"
-$Local:dotnetRuntime = "win10-x64"
+$Local:dotnetRuntime = "portable"
 #debian.8-x64
 
 function XUnitTests([string]$sourcePath, [string]$projectFilter){
     (Get-ChildItem -Path $sourcePath -Recurse -Filter $projectFilter) | ForEach {
-        $outputPath = Join-Path $_.Directory "bin/$dotnetConfiguration/$dotnetFramework/$dotnetRuntime"
+        $outputPath = Join-Path $_.Directory "bin/$dotnetConfiguration/$dotnetFramework"
 
         Exec { & dotnet test --framework $dotnetFramework --configuration $dotnetConfiguration --no-build --output $outputPath --verbosity normal $_.FullName }
     }    
@@ -15,9 +15,11 @@ Task DotnetBuild{
     $sourcePath = Get-Conventions sourcePath
     $projectPath = Join-Path $sourcePath "$($config.applicationName).sln"
 
+    Write-Host "Build runtime: $dotnetRuntime"
+
     Exec { & dotnet clean --framework $dotnetFramework --configuration $dotnetConfiguration --verbosity normal $projectPath }
-    Exec { & dotnet restore --runtime $dotnetRuntime --verbosity normal $projectPath }
-    Exec { & dotnet build --runtime $dotnetRuntime --framework $dotnetFramework --configuration $dotnetConfiguration --verbosity normal $projectPath }
+    Exec { & dotnet restore --verbosity normal $projectPath }
+    Exec { & dotnet build --framework $dotnetFramework --configuration $dotnetConfiguration --verbosity normal $projectPath }
 }
 
 Task DotnetUnitTests{
@@ -40,5 +42,5 @@ Task DotnetPublish {
     $projectPath = Join-Path $sourcePath "$($config.applicationName)/$($config.applicationName).csproj"
     $publishPath = Join-Path $buildPath "published-apps/$($config.applicationName)"
 
-    Exec { & dotnet publish --runtime $dotnetRuntime --framework $dotnetFramework --configuration $dotnetConfiguration --output $publishPath --verbosity normal $projectPath }
+    Exec { & dotnet publish --framework $dotnetFramework --configuration $dotnetConfiguration --output $publishPath --verbosity normal $projectPath }
 }
