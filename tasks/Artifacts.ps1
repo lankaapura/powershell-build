a6ade253475552a93c6488b2a1b7b68599ddc9f2
Stefano Cocchi <kntrashcan@gmail.com>
2017-06-28 02:34:21 +0100

    Added ssh deploy strategy

--- tasks/Artifacts.ps1
+++ tasks/Artifacts.ps1
@@ -5,22 +5,24 @@ Task Artifacts {
 		throw "Artifact configuration not defined"
 	}
 	
-	$artifactSourcePath = Join-Path $buildPath $config.artifact.source
-	$artifactTargetPath = Join-Path $buildPath $config.artifact.name	
+	$sourcePath = Join-Path $buildPath $config.artifact.source
+	$archivePath = Join-Path $buildPath $config.artifact.name	
+	$hashPath = "$($archivePath).SHA256"
 	
-	if(-not (Test-Path $artifactSourcePath)){
-		throw "Artifact source not found: $artifactSourcePath"
+	if(-not (Test-Path $sourcePath)){
+		throw "Artifact source not found: $sourcePath"
 	}	
 	
     $envConfigPath = Join-Path $configPath $environment
     if(Test-Path -PathType Container $envConfigPath){
         Write-Host "Copying environment configuration"
         Get-ChildItem -Path $envConfigPath | ForEach {
-            Write-Host " - $($_.FullName) -> $artifactSourcePath"
-            Copy-Item $_.FullName -Destination $artifactSourcePath -Recurse -Force
+            Write-Host " - $($_.FullName) -> $sourcePath"
+            Copy-Item $_.FullName -Destination $sourcePath -Recurse -Force
         }
     }	
 
-	Write-Host "Creating artifact: $artifactTargetPath"
-	Invoke-ZipCreate $artifactSourcePath $artifactTargetPath
+	Write-Host "Creating artifact: $archivePath"
+	Invoke-ZipCreate $sourcePath $archivePath
+	[System.IO.File]::WriteAllText($hashPath, (Get-FileHash -Path $archivePath -Algorithm SHA256).Hash, [System.Text.Encoding]::ASCII)	
 }
--- tasks/Deploy.ps1
+++ tasks/Deploy.ps1
@@ -1,6 +1,4 @@
-function Deploy-Filesystem($artifactPath, $artifactConfig){
-    Write-Host "Deploying artifact $artifactPath with filesystem strategy"
-
+function Deploy-Filesystem($artifactPath, $hashPath, $artifactConfig){
     if($artifactConfig.path -eq $null -or $artifactConfig.path.length -eq 0){
         throw "Deploy configuration invalid: missing 'path'"
     }
@@ -19,6 +17,24 @@ function Deploy-Filesystem($artifactPath, $artifactConfig){
     Invoke-ZipExtract $artifactPath $artifactConfig.path
 }
 
+function Deploy-Ssh($artifactPath, $hashPath, $artifactConfig){
+    $scpexe = [System.IO.Path]::Combine($PSBuildPath, "vendors", "Putty", "pscp.exe")
+    $privateKeyPath = [System.IO.Path]::Combine($home, '.ssh', "$($artifactConfig.user)@$($artifactConfig.host)")
+
+    if(-not (Test-Path $scpexe)){
+        Write-Host -ForegroundColor Red "SCP not installed: $scpexe"
+        exit 1
+    }
+
+    if(-not (Test-Path $privateKeyPath)){
+        Write-Host -ForegroundColor Red "SSH private  key not found: $privateKeyPath"
+        exit 1
+    }
+
+    Write-Host "Copying artifact to $($artifactConfig.host)"
+	Exec { & $scpexe "-batch" "-q" "-i" "$privateKeyPath" "$artifactPath" "$hashPath" "$($artifactConfig.user)@$($artifactConfig.host):$($artifactConfig.target)" } 
+}
+
 Task Deploy {
     $buildPath = Get-Conventions buildPath
 
@@ -31,13 +47,22 @@ Task Deploy {
 	}		
 	
 	$artifactPath = Join-Path $buildPath $config.artifact.name
+    $hashPath = "$($artifactPath).SHA256"
+
 	if(-not (Test-Path $artifactPath)){
 		throw "Artifact not found: $artifactPath"
 	}
 
+	if(-not (Test-Path $hashPath)){
+		throw "SHA256 file not found: $hashPath"
+	}
+
+	Write-Host "Deploying artifact $artifactPath with $($config.artifact.deploy.strategy) strategy"
+	
 	switch ($config.artifact.deploy.strategy) 
 	{ 
-		"filesystem" { Deploy-Filesystem $artifactPath $config.artifact.deploy }
+		"filesystem" { Deploy-Filesystem $artifactPath $hashPath $config.artifact.deploy }
+		"ssh" { Deploy-Ssh $artifactPath $hashPath $config.artifact.deploy }
 		default { throw "Deploy strategy not implemented: $($artifactConfig.deploy.strategy)" }
 	}
 }
